d1ac43de4386979a113732e5d4881fb6a9f94a79
user name <shegeemankind@gmail.com>
2022-09-12 15:56:28 -0700

    C programming

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,12 @@
 int main(void)
 {
 	int i;
-	for (i =0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(i);
-		if (i != 9)
+		putchar((i % 10) + '0');
+		if (i == 9)
 		{
+			continue;
 			putchar(',');
 			putchar(' ');
 		}
