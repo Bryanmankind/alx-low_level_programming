29a4d443cfe06a7b7a040c0765879ce4407f0a00
user name <shegeemankind@gmail.com>
2022-09-12 14:22:33 -0700

    C programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Print number from 0 to 10.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	for  (i = 0; i < 10; i++)
+	{
+		purchar("%d", i);
+	}
+	purchar('\n');
+	return (0);
+}
