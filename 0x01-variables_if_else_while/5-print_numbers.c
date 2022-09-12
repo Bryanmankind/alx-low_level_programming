104338eac540abc0591d64baceae016b41b4565f
user name <shegeemankind@gmail.com>
2022-09-12 13:44:13 -0700

    C programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - print the digit number of base 10
+ *
+ * return always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d",i);
+	}
+	putchr('\n');
+	return (0);
+}
