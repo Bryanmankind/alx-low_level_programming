d55b011e6606f1781ebab53d9a291a5aa241ae86
user name <shegeemankind@gmail.com>
2022-09-14 15:16:43 -0700

    C function & loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase x10.
+ *
+ * return: void.
+ */
+
+void print_alphabet_x10(void)
+{
+	int alph = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (alph <= 'z')
+		{
+			_putchar(alph++);
+		}
+		i++
+		_putchar('\n');
+	}
+}
