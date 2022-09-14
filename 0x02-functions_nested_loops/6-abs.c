4dddf78db756bca241ed89130aed53826d0772ae
user name <shegeemankind@gmail.com>
2022-09-14 16:21:33 -0700

    C function & loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - calculates absolute value.
+ *
+ * @n: integer parameter
+ *
+ * Return: absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-(n));
+	}
+	else
+	{
+		return (n);
+	}
+}
