9b446bebf55e08e8e584674065c406df25bf4438
user name <shegeemankind@gmail.com>
2022-09-15 16:09:40 -0700

    C - More functions & loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ * mul - multiply two nums together
+ *
+ * @a: integer to multiply by b
+ * @b: integer to multiply by a
+ *
+ * Return: result of a*b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
