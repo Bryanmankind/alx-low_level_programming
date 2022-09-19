f56a67f3111b6893dc93753186936131ca001fb8
user name <shegeemankind@gmail.com>
2022-09-19 11:14:08 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter 
- * and updates the value 98
+ * reset_to_98 - takes a pointer to parameter and updates the value
  *
  * @*n: the pointer to an int.
  */
