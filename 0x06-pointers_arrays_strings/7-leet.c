ed465884f0b63e1bd034b34b6b701b18c3020770
user name <shegeemankind@gmail.com>
2022-09-22 11:53:28 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,4 +5,24 @@
  *
  */
 
+char *leet(char *s)
+{
+	int a = 0; b = 0; l = 5;
+	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
+	char trw[5] = {'4', '3', '0', '7', '1'}
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (b < l)
+		{
+			if (s[a] == tr[b] || s[a] - 32 == tr[b])
+				s[a] = trw[b];
+			b++;
+		}
+		a++;
+	}
+	return (s);
+}
 
