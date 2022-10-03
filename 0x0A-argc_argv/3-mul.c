00cf6dc7f49c39338b0829e927d8a7e40a19c998
user name <shegeemankind@gmail.com>
2022-10-03 13:01:36 -0700

    argc & argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 /**
- * 
  * main - Prints the multiplication of two numbers, followed by a new line
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
