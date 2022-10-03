38780838c57e27ef0a1e9045cf59c308227f139f
user name <shegeemankind@gmail.com>
2022-10-03 12:11:28 -0700

    argc & argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name followed with a new line
+ * @argc: number of argumemt supplied to the program.
+ *@argv: An array of the pointers of the arguments
+ *Returns: (0).
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s/n", argb[0]);
+	return (0);
+}
